a698b9039efd1d74e7909fad5f7e066c5400ff47
FAayzulloh12120212 <13091988@gmail.com>
2025-11-22 13:46:30 +0500

    git push

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-alert("Hallo world")
+// alert("Hallo world")
 
-function qoshgich(a,b){
-    return Math.round(a * b)
-}
-let a = Number(prompt("Birinchi raqam"))
-let b = Number(prompt("Ikkinchi raqam"))
-let natija = qoshgich(a,b);
-alert(`
-    ${natija}
-`)
-if (natija > 0){
-    let togri = confirm("Natija to'gri")
-    console.log(togri)
-}else if(natija < 0){
-    let hato = confirm("Natija hato")
-    console.log(hato)
-}else{
-    let tent = confirm("Natija 0 ga teng")
-    console.log(teng)
-}
+// function qoshgich(a,b){
+//   return Math.round(a + b);  
+// }
+// let a = Number(prompt("Birinchi raqam"));
+// let b = Number(prompt("Ikkinchi raqam"));
+// let natija = qoshgich(a,b);
+// alert(`
+//     ${natija}
+// `)
+
+// if (natija > 0){
+//     let togri = confirm("Qiymat to'gri");
+//     console.log(togri);
+// }else if(natija < 0){
+//     let hato = confirm("Qiymat hato");
+//     console.log(hato);
+// }else{
+//     let tent = confirm("Qiymat 0 ga teng");
+//     console.log(tent);
+// }
