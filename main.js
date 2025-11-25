f07ec70851f47ca69001de564469c3a0342ec20c
FAyzulloh12122012 <13091988@gmail.com>
2025-11-25 17:25:28 +0500

    Tugadi

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-// alert("Hallo world")
+alert("Hallo world")
 
-// function qosh(c,d){
-//     return Math.round(c + d);
-// }
+function qosh(c,d){
+    return Math.round(c + d);
+}
 
-// let c = Number(prompt("Birinchi -"));
-// let d = Number(prompt("Ikkinchi -"));
-// let natijaAyru = qosh(a,b);
-// alert(`
-//     ${natijaAyru}
-//     `)
-// console.log(natijaAyru);    
+let c = Number(prompt("Birinchi -"));
+let d = Number(prompt("Ikkinchi -"));
+let natijaAyru = qosh(a,b);
+alert(`
+    ${natijaAyru}
+    `)
+console.log(natijaAyru);    
 
-// if (natijaAyru > 0){
-//     let togriAyru = confirm("Qiymat to'g'ri");
-//     console.log(togriAyru);
-// }else if(natijaAyru < 0){
-//     let hatoAyru = confirm("Qiymat hato");
-//     console.log(hatoAyru);
-// }else{
-//     let tengAyru = confirm("Qiymat 0 ga teng");
-//     console.log(tengAyru);
-// }
+if (natijaAyru > 0){
+    let togriAyru = confirm("Qiymat to'g'ri");
+    console.log(togriAyru);
+}else if(natijaAyru < 0){
+    let hatoAyru = confirm("Qiymat hato");
+    console.log(hatoAyru);
+}else{
+    let tengAyru = confirm("Qiymat 0 ga teng");
+    console.log(tengAyru);
+}
 
-// function qosh(a,b){
-//     return Math.round(a + b);
-// }
+function qosh(a,b){
+    return Math.round(a + b);
+}
 
-// let a = Number(prompt("Birinchi +"));
-// let b = Number(prompt("Ikkinchi +"));
-// let natija = qosh(a,b);
-// alert(`
-//     ${natija}
-//     `)
-// console.log(natija);    
+let a = Number(prompt("Birinchi +"));
+let b = Number(prompt("Ikkinchi +"));
+let natija = qosh(a,b);
+alert(`
+    ${natija}
+    `)
+console.log(natija);    
 
-// if (natija > 0){
-//     let togri = confirm("Qiymat to'g'ri");
-//     console.log(togri);
-// }else if(natija < 0){
-//     let hato = confirm("Qiymat hato");
-//     console.log(hato);
-// }else{
-//     let teng = confirm("Qiymat 0 ga teng");
-//     console.log(teng);
-// }
+if (natija > 0){
+    let togri = confirm("Qiymat to'g'ri");
+    console.log(togri);
+}else if(natija < 0){
+    let hato = confirm("Qiymat hato");
+    console.log(hato);
+}else{
+    let teng = confirm("Qiymat 0 ga teng");
+    console.log(teng);
+}
 
-// const header = document.getElementById('header');
-// console.log(header);
+const header = document.getElementById('header');
+console.log(header);
 
-// const main = document.getElementById('header');
-// console.log(main);
+const main = document.getElementById('main');
+console.log(main);
 
-// const footer = document.getElementById('header');
-// console.log(footer);
+const footer = document.getElementById('footer');
+console.log(footer);
